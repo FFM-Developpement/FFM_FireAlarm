515485ed203e167bc82266d8c604fbe3049614dc
FFM-Development <96653877+FFM-Developpement@users.noreply.github.com>
2022-06-01 21:28:50 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'FFM FireAlarm'
+
+version '1.0.0'
+
+server_scripts {
+    "server/main.lua",
+    "config.lua",
+} 
+
+client_scripts {
+    "client/main.lua",
+    "config.lua",
+}
