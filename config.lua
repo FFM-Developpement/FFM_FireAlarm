5e8a2e35fb73ee589a3e74dbc8cb21faa930a83c
FFM-Development <96653877+FFM-Developpement@users.noreply.github.com>
2022-06-01 21:29:22 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.FireStations = {
+    { x = -379.53,      y = 6118.32,    z = 31.85,  name = "PB"},   -- Paleto Bay
+    { x = -2095.92,     y = 2830.22,    z = 32.96,  name = "FZ"},   -- Fort Zancudo
+    { x = 1691.24,      y = 3585.83,    z = 35.62,  name = "SS"},   -- Sandy Shores
+    { x = -1068.74,     y = -2379.96,   z = 14.05,  name = "LSIA"}, -- Los Santos Internation Airport
+    { x = 199.83,       y = -1643.38,   z = 29.80,  name = "DAV"},  -- Davis
+    { x = 1193.42,      y = -1473.72,   z = 34.86,  name = "ELB"},  -- El Burro
+    { x = -635.09,      y = -124.29,    z = 39.01,  name = "RH"},   -- Rockford Hills
+}
